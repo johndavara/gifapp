dbc61d93fde913783bac05895aec7d0184725714
Johnnatan Vargas Ramirez <johndavara@gmail.com>
2023-11-19 18:08:51 -0600

    Seccion 8 Image Loader

---
+++ src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
+
+@Component({
+  selector: 'gifs-card',
+  templateUrl: './gifs-card.component.html',
+  styleUrls: ['./gifs-card.component.css']
+})
+export class GifsCardComponent implements OnInit{
+
+  @Input()
+  public gif! : Gif;
+
+  ngOnInit(): void {
+    if(!this.gif){
+      throw new Error('Propiedad gif requerida')
+    }
+  }
+}
